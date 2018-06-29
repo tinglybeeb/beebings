cf5e4df78249ea69aa922e27f6ec6834d66bc3ce
tinglybeeb <tinglybeeb@gmail.com>
2018-06-29 23:10:17 +0800

    Setup automated tests for static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,38 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+	# This function is run before every test
+	def setup
+		@base_title = "| Beebings"
+	end
+
+	# Test that the following pages exist (Test for successful HTTP GET responses)
+	# Test that the pages show the correct titles
+
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
+    assert_select "title", "Home #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Halp #{@base_title}"
   end
+
+  test "should get about" do
+  	get static_pages_about_url
+  	assert_response :success
+  	assert_select "title", "About #{@base_title}"
+  end
+
+  test "should get contact" do
+  	get static_pages_contact_url
+  	assert_response :success
+  	assert_select "title", "Contact Beebs #{@base_title}"
+  end
+
 
 end
